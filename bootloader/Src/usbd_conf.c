5b422adeafbcb93d6c90d5898c8109c1854cc3ce
qsxcv <qsxcv@users.noreply.github.com>
2021-03-26 02:17:35 +0000

    bootloader: change usb to full speed (HS in FS mode), disable vbus detect

--- bootloader/Src/usbd_conf.c
+++ bootloader/Src/usbd_conf.c
@@ -347,8 +347,8 @@ USBD_StatusTypeDef USBD_LL_Init(USBD_HandleTypeDef *pdev)
   hpcd.Init.lpm_enable = 0; 
   hpcd.Init.phy_itface = USB_OTG_HS_EMBEDDED_PHY; 
   hpcd.Init.Sof_enable = 0;
-  hpcd.Init.speed = PCD_SPEED_HIGH;
-  hpcd.Init.vbus_sensing_enable = 1;
+  hpcd.Init.speed = PCD_SPEED_HIGH_IN_FULL;
+  hpcd.Init.vbus_sensing_enable = 0;
   
   /* Link The driver to the stack */
   hpcd.pData = pdev;
