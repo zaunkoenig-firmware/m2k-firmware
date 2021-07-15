zaunkoenig-firmware/m2k-firmware

commit cb32b910cebd3d647085c1809f76d47ceb622cc8

prevent dpi from being set above 12000
previously, it was possible to increase the dpi setting to, for
instance, 12900 by setting dpi to 11900 and using a large step (hold L
and press R). the sensor, however, would run at 12000.

mouse/Src/main.c
@@ -102,7 +102,7 @@ static inline uint32_t mode_process(Config *cfg, int *skip,
 			}
 			if ((released & 0b10) != 0 && cfg->dpi < 0x77) { // RMB released
 				if (btn & 0b01) {
-					cfg->dpi += 10;
+					cfg->dpi += MIN(cfg->dpi + 10, 0x77);
 					anim_lg_updown(1);
 					*large_step = 1;
 				} else {

commit 48b5762082acdc15a9d46b7578a3185f6c33a56d

adjust logic for changing dpi
1. fix bug introduced in cb32b91
2. previously, trying to do a large decrease at 100dpi would result in
   increasing 100dpi when right button is released, because *large_step
   was not set.

mouse/Src/main.c
@@ -80,37 +80,44 @@ static inline uint32_t mode_process(Config *cfg, int *skip,
 
 	int lifted = (squal < 16); // lifted and sensor not tracking
 
+	const int dpi_min = 0x00; // 0 = 100dpi
+	const int dpi_max = 0x77; // 127 = 12000dpi
+
 	// if the user's continued to hold buttons after a state transition has happened, don't act on those clicks as if they were new
 	if (!*holding_transitioned) {
 		if (mode == 1) { // handle cpi mode
 			const uint8_t released = (~btn) & btn_prev;
-			if ((released & 0b01) != 0 && cfg->dpi > 0x00 && lifted) { // LMB released
-				if (btn & 0b10) {
-					cfg->dpi = MAX(cfg->dpi - 10, 0);
-					anim_lg_downup(1);
-					*large_step = 1;
-				} else {
-					if (!*large_step) {
-						cfg->dpi -= 1;
-						anim_downup(1);
-					} else {
-						*large_step = 0;
+			if ((released & 0b01) != 0 && lifted) { // LMB released
+				if (btn & 0b10) { // if RMB is held
+					if (cfg->dpi != dpi_min) {
+						cfg->dpi = MAX(cfg->dpi - 10, dpi_min);
+						anim_lg_downup(1);
 					}
+					*large_step = 1;
+				} else if (!*large_step) {
+					if (cfg->dpi != dpi_min) {
+						cfg->dpi = MAX(cfg->dpi - 1, dpi_min);
+						anim_downup(1);
+					}
+				} else {
+					*large_step = 0;
 				}
 				pmw3360_set_dpi(cfg->dpi);
 			}
-			if ((released & 0b10) != 0 && cfg->dpi < 0x77 && lifted) { // RMB released
-				if (btn & 0b01) {
-					cfg->dpi += MIN(cfg->dpi + 10, 0x77);
-					anim_lg_updown(1);
-					*large_step = 1;
-				} else {
-					if (!*large_step) {
-						cfg->dpi++;
-						anim_updown(1);
-					} else {
-						*large_step = 0;
+			if ((released & 0b10) != 0 && lifted) { // RMB released
+				if (btn & 0b01) { // if LMB is held
+					if (cfg->dpi != dpi_max) {
+						cfg->dpi = MIN(cfg->dpi + 10, dpi_max);
+						anim_lg_updown(1);
 					}
+					*large_step = 1;
+				} else if (!*large_step) {
+					if (cfg->dpi != dpi_max) {
+						cfg->dpi = MIN(cfg->dpi + 1, dpi_max);
+						anim_updown(1);
+					}
+				} else {
+					*large_step = 0;
 				}
 				pmw3360_set_dpi(cfg->dpi);
 			}