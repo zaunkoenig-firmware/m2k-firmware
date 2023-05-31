f46a3a21fcaa277b67b8543a53ad804e883b52eb
Ryan Lewis <ryan@rlew.io>
2023-05-31 14:01:51 +0100

    Fix constant usage in LMB/RMB constants

--- mouse/Inc/lefty_mode.h
+++ mouse/Inc/lefty_mode.h
@@ -27,24 +27,24 @@
 #define LMB_NO_CLK_ENABLE() do {RCC->AHB1ENR |= RCC_AHB1ENR_GPIOEEN;} while(0)
 #define LMB_NO_PORT         GPIOE
 #define LMB_NO_PIN_Pos      4
-#define LMB_NO_PIN          (1 << RMB_NO_PIN_Pos)
+#define LMB_NO_PIN          (1 << LMB_NO_PIN_Pos)
 #define LMB_NO_EXTICFG      SYSCFG_EXTICR2_EXTI4_PE
 
 #define LMB_NC_CLK_ENABLE() do {RCC->AHB1ENR |= RCC_AHB1ENR_GPIOEEN;} while(0)
 #define LMB_NC_PORT         GPIOE
 #define LMB_NC_PIN_Pos      15
-#define LMB_NC_PIN          (1 << RMB_NC_PIN_Pos)
+#define LMB_NC_PIN          (1 << LMB_NC_PIN_Pos)
 #define LMB_NC_EXTICFG      SYSCFG_EXTICR4_EXTI15_PE
 
 // rmb, but with pins for lmb
 #define RMB_NO_CLK_ENABLE() do {RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;} while(0)
 #define RMB_NO_PORT         GPIOD
 #define RMB_NO_PIN_Pos      10
-#define RMB_NO_PIN          (1 << LMB_NO_PIN_Pos)
+#define RMB_NO_PIN          (1 << RMB_NO_PIN_Pos)
 #define RMB_NO_EXTICFG      SYSCFG_EXTICR3_EXTI10_PD
 
 #define RMB_NC_CLK_ENABLE() do {RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;} while(0)
 #define RMB_NC_PORT         GPIOD
 #define RMB_NC_PIN_Pos      8
-#define RMB_NC_PIN          (1 << LMB_NC_PIN_Pos)
+#define RMB_NC_PIN          (1 << RMB_NC_PIN_Pos)
 #define RMB_NC_EXTICFG      SYSCFG_EXTICR3_EXTI8_PD
